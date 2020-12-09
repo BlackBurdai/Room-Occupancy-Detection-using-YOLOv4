6597a12d2fd494c2fcd7f9720b953eeb076e683d
sakshamprakash01 <sakshamprakash01@gmail.com>
2020-12-09 09:07:49 +0530

    added sheets api support

--- script1.py
+++ script1.py
@@ -6,6 +6,12 @@ import os
 from centroidtracker import CentroidTracker
 import copy
 
+import datetime
+from datetime import timedelta
+
+from sheetsAPI import insertRecord
+
+
 tracker = CentroidTracker(maxDisappeared=20, maxDistance=90)
 
 CONFIDENCE = 0.5
@@ -18,8 +24,13 @@ labels = open("data/coco.names").read().strip().split("\n")
 
 net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 
-path_name = "test_videos/muskan2.mp4"
+path_name = "test_videos/20201207_205700.mp4"
 cap = cv2.VideoCapture(path_name)
+
+basename = os.path.basename(path_name)
+filename = os.path.splitext(basename)[0]
+
+clock = datetime.datetime(year=int(filename[0:4]), month=int(filename[4:6]), day=int(filename[6:8]), hour=int(filename[9:11]), minute=int(filename[11:13]), second=int(filename[13:15]))
 
 video_framerate = cap.get(cv2.CAP_PROP_FPS)
 
@@ -34,8 +45,9 @@ out = cv2.VideoWriter("output2.mp4", fourcc, video_framerate, (1920, 1080), True
 fps = 0
 frameNo = 0
 
-rooms=[{"MinX":1118,"MaxX":1600,"MinY":115,"MaxY":971},{"MinX":266,"MaxX":480,"MinY":141,"MaxY":680}]
-occupancy = [0,0]
+#rooms = [{"MinX": 1118, "MaxX": 1600, "MinY": 115, "MaxY": 971}, {"MinX": 266, "MaxX": 480, "MinY": 141, "MaxY": 680}] #SCHOOL VIDEO
+rooms = [{"MinX": 1190, "MaxX": 1316, "MinY": 323, "MaxY": 998}, {"MinX": 902, "MaxX": 1043, "MinY": 265, "MaxY": 605}]  #MUSKAAN HOME
+occupancy = [0, 0]
 entry_exit = {}
 
 # cv2.namedWindow('controls')
@@ -51,6 +63,20 @@ while True:
     if not ret:
         break
     # image = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+    h, w = image.shape[:2]
+
+    for i, room in enumerate(rooms):
+        cv2.rectangle(image, (room["MinX"], room["MinY"]), (room["MaxX"], room["MaxY"]), (255, 0, 0), 3)
+
+    cv2.putText(
+        image,
+        str(clock)[:-4],
+        (5, 65),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2,
+    )
 
     # MinX = cv2.getTrackbarPos('MinX','controls')
     # MinY = cv2.getTrackbarPos('MinY','controls')
@@ -58,16 +84,10 @@ while True:
     # MaxY = cv2.getTrackbarPos('MaxY','controls')
 
     # image = image[MinY:MaxY, MinX:MaxX]
-    for i,room in enumerate(rooms): 
-        cv2.rectangle(image, (room["MinX"], room["MinY"]), (room["MaxX"], room["MaxY"]), (255, 0, 0), 1)
-        cv2.putText(image, "Room-{n}".format(n=i+1), (room["MinX"], room["MinY"]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1,)
-
     image_cpy = copy.deepcopy(image)
 
     unique_count = 0
     current_count = 0
-
-    h, w = image.shape[:2]
 
     # DwellYLevel = 500
     # cv2.line(image, (0, DwellYLevel), (1920, DwellYLevel), (255, 128, 0), 2)
@@ -125,8 +145,6 @@ while True:
 
     objects, dereg = tracker.update(rects)
 
-    print(dereg)
-    print()
     for (objectId, bbox) in objects.items():
         x1, y1, x2, y2 = bbox
         x1 = abs(int(x1))
@@ -144,36 +162,36 @@ while True:
             object_id_list.append(objectId)  # Append in ObjectID list - list of ID of people who have come before
             unique_count += 1
 
-            if (centroidX >= rooms[0]["MinX"] and centroidX <= rooms[0]["MaxX"] and centroidY >= rooms[0]["MinY"] and centroidY <= rooms[0]["MaxY"]):
-                key=objectId
-                value = [1,0,0] # [R1,R2,O]
-                entry_exit[key]=value
-            
-            elif (centroidX >= rooms[1]["MinX"] and centroidX <= rooms[1]["MaxX"] and centroidY >= rooms[1]["MinY"] and centroidY <= rooms[1]["MaxY"]):
-                key=objectId
-                value = [0,1,0] # [R1,R2,O]
-                entry_exit[key]=value
+            if centroidX >= rooms[0]["MinX"] and centroidX <= rooms[0]["MaxX"] and centroidY >= rooms[0]["MinY"] and centroidY <= rooms[0]["MaxY"]:
+                key = objectId
+                value = [1, 0, 0]  # [R1,R2,O]
+                entry_exit[key] = value
+
+            elif centroidX >= rooms[1]["MinX"] and centroidX <= rooms[1]["MaxX"] and centroidY >= rooms[1]["MinY"] and centroidY <= rooms[1]["MaxY"]:
+                key = objectId
+                value = [0, 1, 0]  # [R1,R2,O]
+                entry_exit[key] = value
             else:
-                key=objectId
-                value = [0,0,1] # [R1,R2,O]
-                entry_exit[key]=value
-            
+                key = objectId
+                value = [0, 0, 1]  # [R1,R2,O]
+                entry_exit[key] = value
+
         else:
 
-            if (centroidX >= rooms[0]["MinX"] and centroidX <= rooms[0]["MaxX"] and centroidY >= rooms[0]["MinY"] and centroidY <= rooms[0]["MaxY"]):
+            if centroidX >= rooms[0]["MinX"] and centroidX <= rooms[0]["MaxX"] and centroidY >= rooms[0]["MinY"] and centroidY <= rooms[0]["MaxY"]:
                 pass
-            
-            elif (centroidX >= rooms[1]["MinX"] and centroidX <= rooms[1]["MaxX"] and centroidY >= rooms[1]["MinY"] and centroidY <= rooms[1]["MaxY"]):
+
+            elif centroidX >= rooms[1]["MinX"] and centroidX <= rooms[1]["MaxX"] and centroidY >= rooms[1]["MinY"] and centroidY <= rooms[1]["MaxY"]:
                 pass
             else:
-                if (entry_exit[objectId][0]==1):
-                    occupancy[0]-=1
-                    entry_exit[objectId][0]=0
-                elif (entry_exit[objectId][1]==1):
-                    occupancy[1]-=1
-                    entry_exit[objectId][1]=0
-
-        
+                if entry_exit[objectId][0] == 1:
+                    occupancy[0] -= 1
+                    insertRecord([str(clock), str(occupancy[0]), str(occupancy[1])])
+                    entry_exit[objectId][0] = 0
+                elif entry_exit[objectId][1] == 1:
+                    occupancy[1] -= 1
+                    insertRecord([str(clock), str(occupancy[0]), str(occupancy[1])])
+                    entry_exit[objectId][1] = 0
 
         ID_text = "Person:" + str(objectId)
 
@@ -190,27 +208,28 @@ while True:
         centroidX = int((x1 + x2) / 2)
         centroidY = int((y1 + y2) / 2)
 
-        if (centroidX >= rooms[0]["MinX"] and centroidX <= rooms[0]["MaxX"] and centroidY >= rooms[0]["MinY"] and centroidY <= rooms[0]["MaxY"] and entry_exit[objectID][0]!=1):
-            occupancy[0]+=1
+        if centroidX >= rooms[0]["MinX"] and centroidX <= rooms[0]["MaxX"] and centroidY >= rooms[0]["MinY"] and centroidY <= rooms[0]["MaxY"] and entry_exit[objectID][0] != 1:
+            occupancy[0] += 1
             del entry_exit[objectID]
+            insertRecord([str(clock), str(occupancy[0]), str(occupancy[1])])
             tracker.deleteDereg(objectID)
-        elif (centroidX >= rooms[1]["MinX"] and centroidX <= rooms[1]["MaxX"] and centroidY >= rooms[1]["MinY"] and centroidY <= rooms[1]["MaxY"] and entry_exit[objectID][1]!=1):
-            occupancy[1]+=1
+        elif centroidX >= rooms[1]["MinX"] and centroidX <= rooms[1]["MaxX"] and centroidY >= rooms[1]["MinY"] and centroidY <= rooms[1]["MaxY"] and entry_exit[objectID][1] != 1:
+            occupancy[1] += 1
             del entry_exit[objectID]
+            insertRecord([str(clock), str(occupancy[0]), str(occupancy[1])])
             tracker.deleteDereg(objectID)
         else:
             del entry_exit[objectID]
             tracker.deleteDereg(objectID)
-
 
     total_count += unique_count
     total_count_text = "Total Count:" + str(total_count)
     cv2.putText(
         image,
         total_count_text,
-        (5, 400),
+        (5, 500),
         cv2.FONT_HERSHEY_SIMPLEX,
-        1.5,
+        1,
         (0, 0, 255),
         2,
     )
@@ -219,29 +238,29 @@ while True:
     cv2.putText(
         image,
         current_count_text,
-        (5, 450),
+        (5, 535),
         cv2.FONT_HERSHEY_SIMPLEX,
-        1.5,
+        1,
         (0, 0, 255),
         2,
     )
 
-    occupancy1_text = "Room1: " + str(occupancy[0]) + " People"
+    occupancy1_text = "Room A: " + str(occupancy[0])
     cv2.putText(
         image,
         occupancy1_text,
-        (5, 500),
+        (rooms[0]["MinX"], rooms[0]["MinY"]),
         cv2.FONT_HERSHEY_SIMPLEX,
         1.5,
         (0, 0, 255),
         2,
     )
 
-    occupancy2_text = "Room2: " + str(occupancy[1]) + " People"
+    occupancy2_text = "Room B: " + str(occupancy[1])
     cv2.putText(
         image,
         occupancy2_text,
-        (5, 550),
+        (rooms[1]["MinX"], rooms[1]["MinY"]),
         cv2.FONT_HERSHEY_SIMPLEX,
         1.5,
         (0, 0, 255),
@@ -249,8 +268,10 @@ while True:
     )
 
     end_time = time.time()
-    fps = 1 / (end_time - start_time)
+    frame_time = end_time - start_time
+    fps = 1 / frame_time
     fps_text = "FPS: {:.2f}".format(fps)
+    clock += datetime.timedelta(milliseconds=frame_time * 100)
     cv2.putText(image, fps_text, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     frameNo += 1
 
